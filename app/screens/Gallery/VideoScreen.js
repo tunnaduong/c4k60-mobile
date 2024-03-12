3b11568df51fef24db2308330fe71fc25a655fb0
Duong Tung Anh <tunnaduong@gmail.com>
2024-03-12 12:50:50 +0700

    update video screen

--- app/screens/Gallery/VideoScreen.js
+++ app/screens/Gallery/VideoScreen.js
@@ -5,12 +5,90 @@ import {
   ScrollView,
   ActivityIndicator,
   TouchableOpacity,
+  StyleSheet,
 } from "react-native";
 import axios from "axios";
 import { Image } from "expo-image";
+import { Video, ResizeMode } from "expo-av";
+import Modal from "react-native-modal";
+import YoutubePlayer from "react-native-youtube-iframe";
+
+const VideoPopup = ({
+  visible,
+  url,
+  onRequestClose,
+  caption,
+  type = "html5",
+}) => {
+  return (
+    <>
+      {type == "html5" ? (
+        <Modal
+          onBackdropPress={onRequestClose}
+          isVisible={visible}
+          backdropOpacity={1}
+        >
+          <TouchableOpacity style={styles.closeButton} onPress={onRequestClose}>
+            <Text style={styles.closeText}>✕</Text>
+          </TouchableOpacity>
+          <Video
+            style={styles.video}
+            source={{
+              uri: url,
+            }}
+            useNativeControls
+            resizeMode={Video.RESIZE_MODE_CONTAIN}
+            isLooping
+            shouldPlay
+          />
+          <Text
+            style={{
+              fontSize: 18,
+              color: "#FFF",
+              textAlign: "center",
+              marginTop: 30,
+            }}
+          >
+            {caption}
+          </Text>
+        </Modal>
+      ) : (
+        <>
+          <Modal
+            onBackdropPress={onRequestClose}
+            isVisible={visible}
+            backdropOpacity={1}
+          >
+            <TouchableOpacity
+              style={styles.closeButton}
+              onPress={onRequestClose}
+            >
+              <Text style={styles.closeText}>✕</Text>
+            </TouchableOpacity>
+            <YoutubePlayer height={300} play={true} videoId={url} />
+            <Text
+              style={{
+                fontSize: 18,
+                color: "#FFF",
+                textAlign: "center",
+                marginTop: 30,
+              }}
+            >
+              {caption}
+            </Text>
+          </Modal>
+        </>
+      )}
+    </>
+  );
+};
 
 export default function VideoScreen() {
   const [videoList, setVideoList] = React.useState(null);
+  const [visible, setVisible] = React.useState(false);
+  const [videoUrl, setVideoUrl] = React.useState("");
+  const [caption, setCaption] = React.useState("");
+  const [type, setType] = React.useState("html5");
 
   React.useEffect(() => {
     getVideoList();
@@ -93,6 +171,17 @@ export default function VideoScreen() {
                       padding: 2,
                     }}
                     activeOpacity={0.6}
+                    onPress={() => {
+                      setVideoUrl(
+                        item.link_youtube.replace(
+                          "https://www.youtube.com/watch?v=",
+                          ""
+                        )
+                      );
+                      setVisible(true);
+                      setCaption(item.caption);
+                      setType("youtube");
+                    }}
                   >
                     <Image
                       source={
@@ -208,9 +297,56 @@ export default function VideoScreen() {
                   </TouchableOpacity>
                 ))}
             </View>
+            <VideoPopup
+              visible={visible}
+              url={videoUrl}
+              onRequestClose={() => setVisible(false)}
+              caption={caption}
+              type={type}
+            />
           </ScrollView>
         </>
       )}
     </>
   );
 }
+
+const styles = StyleSheet.create({
+  background: {
+    position: "absolute",
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    backgroundColor: "black",
+  },
+  videoContainer: {
+    position: "absolute",
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  video: {
+    alignSelf: "center",
+    width: "100%",
+    height: 240,
+  },
+  closeButton: {
+    width: 45,
+    height: 45,
+    position: "absolute",
+    right: 0,
+    zIndex: 1000,
+    top: 50,
+  },
+  closeText: {
+    lineHeight: 25,
+    fontSize: 25,
+    paddingTop: 2,
+    includeFontPadding: false,
+    color: "#FFF",
+  },
+});
