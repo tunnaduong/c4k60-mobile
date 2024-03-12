662eee3fce9869ce15f3c31034ba8df58fac09e8
Duong Tung Anh <tunnaduong@gmail.com>
2024-03-12 10:00:14 +0700

    nnn

--- app/screens/Gallery/VideoScreen.js
+++ app/screens/Gallery/VideoScreen.js
@@ -5,92 +5,12 @@ import {
   ScrollView,
   ActivityIndicator,
   TouchableOpacity,
-  Pressable,
-  StyleSheet,
-  Alert,
 } from "react-native";
 import axios from "axios";
 import { Image } from "expo-image";
-import { Video, ResizeMode } from "expo-av";
-import Modal from "react-native-modal";
-import YoutubePlayer from "react-native-youtube-iframe";
-
-const VideoPopup = ({
-  visible,
-  url,
-  onRequestClose,
-  caption,
-  type = "html5",
-}) => {
-  return (
-    <>
-      {type == "html5" ? (
-        <Modal
-          onBackdropPress={onRequestClose}
-          isVisible={visible}
-          backdropOpacity={1}
-        >
-          <TouchableOpacity style={styles.closeButton} onPress={onRequestClose}>
-            <Text style={styles.closeText}>✕</Text>
-          </TouchableOpacity>
-          <Video
-            style={styles.video}
-            source={{
-              uri: url,
-            }}
-            useNativeControls
-            resizeMode={Video.RESIZE_MODE_CONTAIN}
-            isLooping
-            shouldPlay
-          />
-          <Text
-            style={{
-              fontSize: 18,
-              color: "#FFF",
-              textAlign: "center",
-              marginTop: 30,
-            }}
-          >
-            {caption}
-          </Text>
-        </Modal>
-      ) : (
-        <>
-          <Modal
-            onBackdropPress={onRequestClose}
-            isVisible={visible}
-            backdropOpacity={1}
-          >
-            <TouchableOpacity
-              style={styles.closeButton}
-              onPress={onRequestClose}
-            >
-              <Text style={styles.closeText}>✕</Text>
-            </TouchableOpacity>
-            <YoutubePlayer height={300} play={true} videoId={url} />
-            <Text
-              style={{
-                fontSize: 18,
-                color: "#FFF",
-                textAlign: "center",
-                marginTop: 30,
-              }}
-            >
-              {caption}
-            </Text>
-          </Modal>
-        </>
-      )}
-    </>
-  );
-};
 
 export default function VideoScreen() {
   const [videoList, setVideoList] = React.useState(null);
-  const [visible, setVisible] = React.useState(false);
-  const [videoUrl, setVideoUrl] = React.useState("");
-  const [caption, setCaption] = React.useState("");
-  const [type, setType] = React.useState("html5");
 
   React.useEffect(() => {
     getVideoList();
@@ -173,17 +93,6 @@ export default function VideoScreen() {
                       padding: 2,
                     }}
                     activeOpacity={0.6}
-                    onPress={() => {
-                      setVideoUrl(
-                        item.link_youtube.replace(
-                          "https://www.youtube.com/watch?v=",
-                          ""
-                        )
-                      );
-                      setVisible(true);
-                      setCaption(item.caption);
-                      setType("youtube");
-                    }}
                   >
                     <Image
                       source={
@@ -258,6 +167,7 @@ export default function VideoScreen() {
                       padding: 2,
                     }}
                     activeOpacity={0.6}
+<<<<<<< HEAD
                     onPress={() => {
                       setVideoUrl(
                         "https://c4k60.tunnaduong.com/anhvavideo/" + item.path
@@ -266,6 +176,8 @@ export default function VideoScreen() {
                       setCaption(item.caption);
                       setType("html5");
                     }}
+=======
+>>>>>>> parent of 7defb22 (OK)
                   >
                     <Image
                       source={
@@ -299,56 +211,9 @@ export default function VideoScreen() {
                   </TouchableOpacity>
                 ))}
             </View>
-            <VideoPopup
-              visible={visible}
-              url={videoUrl}
-              onRequestClose={() => setVisible(false)}
-              caption={caption}
-              type={type}
-            />
           </ScrollView>
         </>
       )}
     </>
   );
 }
-
-const styles = StyleSheet.create({
-  background: {
-    position: "absolute",
-    top: 0,
-    left: 0,
-    right: 0,
-    bottom: 0,
-    backgroundColor: "black",
-  },
-  videoContainer: {
-    position: "absolute",
-    top: 0,
-    left: 0,
-    right: 0,
-    bottom: 0,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  video: {
-    alignSelf: "center",
-    width: "100%",
-    height: 240,
-  },
-  closeButton: {
-    width: 45,
-    height: 45,
-    position: "absolute",
-    right: 0,
-    zIndex: 1000,
-    top: 50,
-  },
-  closeText: {
-    lineHeight: 25,
-    fontSize: 25,
-    paddingTop: 2,
-    includeFontPadding: false,
-    color: "#FFF",
-  },
-});
